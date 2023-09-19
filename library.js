62675c96f588d183a0d50d2303f98ee2e5f62158
George-Gabechava <georgegabechava@gmail.com>
2023-09-19 09:50:36 -0400

    completed 4/6 tasks

--- library.js
+++ library.js
@@ -1,7 +1,7 @@
 console.log('Hello Worlds');
 
 //Library
-const myLibrary = [];
+let myLibrary = [];
 
 function Book(Title, Author, Pages, Read) {
   this.Title = Title;
@@ -16,18 +16,7 @@ function addBookToLibrary(Title, Author, Pages, Read) {
   myLibrary.push(aBook);
 }
 
-
-//Example books
-
-addBookToLibrary('Sapiens: A Brief History of Humankind', 
-'Yuval Noah Harari', '464', 'no')
-
-addBookToLibrary('Antifragile: Things That Gain from Disorder', 
-'Nassim Nicholas Taleb', '545','no');
-
-console.log(myLibrary);
-
-/////////////////////////////////// Create Table
+/////// Create Table
 function createTableFromObjects(data) {
   const table = document.createElement('table');
   const headerRow = document.createElement('tr');
@@ -55,9 +44,24 @@ function createTableFromObjects(data) {
   return table;
 }
 
-const mytable = createTableFromObjects(myLibrary);
+//Example books
+
+addBookToLibrary('Example book', 
+'Example Author', '0', 'N/A');
+
+// addBookToLibrary('Sapiens: A Brief History of Humankind', 
+// 'Yuval Noah Harari', '464', 'no');
+
+// addBookToLibrary('Antifragile: Things That Gain from Disorder', 
+// 'Nassim Nicholas Taleb', '545','no');
+
+
+let mytable = createTableFromObjects(myLibrary);
 const tableContainer = document.getElementById('table-container');
+mytable.deleteRow(-1);
 tableContainer.appendChild(mytable);
+myLibrary = [];
+
 
 
 
@@ -69,27 +73,33 @@ let currentStatus = '';
 function myFunction(statusButton) {
   document.getElementsByName("status").value = statusButton;
   currentStatus = statusButton;
-  console.log(currentStatus);
+  // console.log(currentStatus);
 }
 const confirmBtn = favDialog.querySelector("#confirmBtn");
+const bookTitle = document.querySelector('#fTitle');
 
 // "Show the dialog" button opens the <dialog> modally
 showButton.addEventListener("click", () => {
   favDialog.showModal();
 });
 
-// "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
-favDialog.addEventListener("close", (e) => {
-  currentStatus = '';
-  outputBox.value =
-    favDialog.returnValue === "default"
-      ? "No return value."
-      : `ReturnValue: ${favDialog.returnValue}.`; // Have to check for "default" rather than empty string
-});
+// // "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
+// favDialog.addEventListener("close", (e) => {
+//   currentStatus = '';
+//   // outputBox.value =
+//   //   favDialog.returnValue === "default"
+//   //     ? "No return value."
+//   //     : `ReturnValue: ${favDialog.returnValue}.`; // Have to check for "default" rather than empty string
+// });
 
 // Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
 confirmBtn.addEventListener("click", (event) => {
   event.preventDefault(); // We don't want to submit this fake form
-  console.log('currentStatus:', currentStatus);
-  addBookToLibrary('filler', 'filler', 'filler', currentStatus);
+  favDialog.close();
+  addBookToLibrary(document.getElementById("fTitle").value, document.getElementById("fauthor").value, document.getElementById("fpages").value, currentStatus);
+  mytable.remove();
+  mytable = createTableFromObjects(myLibrary);
+  createTableFromObjects(myLibrary);
+  tableContainer.appendChild(mytable);
+
 });
